d67496b5deb432561167b2f7415900006b5e9a39
leshax <leshax@gmail.com>
2019-03-29 12:20:06 +0100

    Playing around with HOOKS

--- message-webhook.js
+++ message-webhook.js
@@ -1,11 +1,17 @@
 const processMessage = require('./process-message');
 module.exports = (req, res) => {
+  console.log("HOOK!");
   if (req.body.object === 'page') {
+    //console.log("-", req.body.object);
     req.body.entry.forEach(entry => {
+      //console.log("--", JSON.stringify(entry));
       entry.messaging.forEach(event => {
         if (event.message && event.message.text) {
-          console.log("TEXT: ", event.message.text);
+          console.log("---", "processMessage");
           processMessage(event);
+        } else if(event.postback && event.postback.payload === "Greeting") {
+          console.log("---", "greetingMessage");
+          //processGreeting(event);
         }
       });
     });
--- process-message.js
+++ process-message.js
@@ -13,8 +13,7 @@ const config = {
     client_email: process.env.DIALOGFLOW_CLIENT_EMAIL
   }
 };
-console.log("Credentials");
-console.log(config);
+
 const sessionClient = new dialogflow.SessionsClient(config);
 
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
@@ -44,6 +43,33 @@ const sendTextMessage = (userId, text) => {
   );
 }
 
+const sendButtons = (userId, text, buttons) => {
+   return fetch(
+    `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        messaging_type: 'RESPONSE',
+        recipient: {
+          id: userId,
+        },
+        message: {
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"button",
+              "text": text,
+              "buttons": buttons
+          }
+        }
+      }
+    }),
+  }); 
+}
+
 module.exports = (event) => {
   const userId = event.sender.id;
   const message = event.message.text;
@@ -61,11 +87,53 @@ module.exports = (event) => {
   sessionClient
     .detectIntent(request)
     .then(responses => {
-      console.log("Sending to Dialogflow...");
+      //console.log(JSON.stringify(myObject, null, 4));
+      console.log("Recieving from Dialogflow...");      
       const result = responses[0].queryResult;
       return sendTextMessage(userId, result.fulfillmentText);
+    }).then(responses => {
+      /*console.log("Sending buttons...");
+      let buttons = [
+               {
+                "type":"postback",
+                "payload":"data",
+                "title":"Show all reminders"
+              },  
+              {
+                "type":"postback",
+                "payload":"data",
+                "title":"Delete all reminders"
+              }     
+            ];            
+            /*
+        let button = [{
+            "type":"web_url",
+            "url":"https://petersapparel.parseapp.com",
+            "title":"Show Website"
+          }];
+        */
+      //return sendButtons(userId, "Choose options", buttons);
     })
     .catch(err => {
       console.error('!ERROR:', err);
     });
 }
+
+/*
+
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type":"call_to_actions",
+  "thread_state":"new_thread",
+  "call_to_actions":[
+    {
+      "payload":"Greeting"
+    }
+  ]
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAJTOmYGRy4BAKEPdhgQVZAEuFPfRxYVdzScMC8UVFGPb2ejm0hahh6bw2wv7RZBqB7ymEVWRZAEwYwgEOd5cgzFIMJgyPVyBbFx656xm8Bgc227TKSZA6TfZAynNgix8nZCXG6sO1G33dXEXCsZCosj2UtXT4JisE4WFDPdE7YUwZDZD"
+
+curl -X POST -H "Content-Type: application/json" -d '{
+  "get_started": {"payload": "<postback_payload>"}
+}'
+ "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=<PAGE_ACCESS_TOKEN>"
+
+*/
